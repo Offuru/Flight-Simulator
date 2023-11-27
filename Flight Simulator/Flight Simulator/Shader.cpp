4567d4ce247155ef2d8c5744b86fd5b1f0a53807
Offuru <andrei.rozmarin@gmail.com>
2023-11-27 16:43:34 +0200

    Implemented Shader class methods

---
+++ Flight Simulator/Flight Simulator/Shader.cpp
@@ -0,0 +1,93 @@
+#include "Shader.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+Shader::Shader(const std::string& vertexPath, const std::string& fragmentPath)
+{
+
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::ifstream vertexShaderFile;
+	std::ifstream fragmentShaderFile;
+
+	vertexShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fragmentShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try {
+		// open files
+		vertexShaderFile.open(vertexPath);
+		fragmentShaderFile.open(fragmentPath);
+		std::stringstream vShaderStream, fShaderStream;
+		// read file's buffer contents into streams
+		vShaderStream << vertexShaderFile.rdbuf();
+		fShaderStream << fragmentShaderFile.rdbuf();
+		// close file handlers
+		vertexShaderFile.close();
+		fragmentShaderFile.close();
+		// convert stream into string
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+	}
+	catch (std::ifstream::failure e) {
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+	const char* vertexShaderCode = vertexCode.c_str();
+	const char* fragmentShaderCode = fragmentCode.c_str();
+
+
+	GLuint vertex, fragment;
+
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vertexShaderCode, NULL);
+	glCompileShader(vertex);
+	CheckCompileErrors(vertex, "VERTEX");
+
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fragmentShaderCode, NULL);
+	glCompileShader(fragment);
+	CheckCompileErrors(fragment, "FRAGMENT");
+
+	m_ID = glCreateProgram();
+	glAttachShader(m_ID, vertex);
+	glAttachShader(m_ID, fragment);
+	glLinkProgram(m_ID);
+	CheckCompileErrors(m_ID, "PROGRAM");
+
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+Shader::~Shader()
+{
+	glDeleteProgram(m_ID);
+}
+
+void Shader::Use() const
+{
+	glUseProgram(m_ID);
+}
+
+GLuint Shader::GetID() const
+{
+	return m_ID;
+}
+
+void Shader::CheckCompileErrors(GLuint shader, const std::string& type)
+{
+	GLint success;
+	GLchar infoLog[1024];
+	if (type != "PROGRAM") {
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+	else {
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success) {
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+}
