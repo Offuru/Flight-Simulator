fb85e6733f69c988fd857d00b142d174fc594072
Offuru <andrei.rozmarin@gmail.com>
2023-12-26 20:52:39 +0200

    Added shader files

--- Flight Simulator/Flight Simulator/PhongLight.fs
+++ Flight Simulator/Flight Simulator/PhongLight.fs
@@ -3,29 +3,35 @@ out vec4 FragColor;
 
 in vec3 Normal;  
 in vec3 FragPos;  
-
+  
 uniform vec3 lightPos; 
 uniform vec3 viewPos; 
 uniform vec3 lightColor;
 uniform vec3 objectColor;
 
-
-uniform float Ka; //ambience constant
-uniform float Kd; //diffusion constant
-uniform float Ks; //specular constant
-uniform float spec;
-
 void main()
 {
+	// simple color blending
+    //FragColor = vec4(lightColor * objectColor, 1.0);
+	
+	// ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+  	
+    // diffuse
+	float diffuseStrength = 0.5;
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = Kd * max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diffuseStrength * diff * lightColor;
+    
+    // specular
+    float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    vec3 specular = Ks * lightColor * pow(max(dot(viewDir, reflectDir), 0.0), spec);
-
-    FragColor = vec4((Ka + diffuse + specular) * objectColor, 1.0);
-
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;  
+        
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
 } 
